85c2e1f4ecb74f42b97661dd3d255c74d51dac4d
Mark Boyd <10804241+markalanboyd@users.noreply.github.com>
2023-04-10 19:32:49 +0200

    Fixed problem with vec2 inputs
    Audulus can't take vec2's as inputs - only floats. Simple fix, just need to split them apart as inputs then later combine them into vec2s if necessary.

--- documentation/book_of_shaders/03_b_gl_FragCoord/03_b_gl_FragCoord.glsl
+++ documentation/book_of_shaders/03_b_gl_FragCoord/03_b_gl_FragCoord.glsl
@@ -5,12 +5,14 @@
 #version 450
 layout(location = 0) out vec4 outColor;
 layout(binding = 0) uniform Inputs {
-    vec2 u_resolution;
-    vec2 u_mouse;
+    float u_resolution;
+    float u_pad_x;
+    float u_pad_y;
     float u_time;
 } inputs;
 
 void main() {
     vec2 st = gl_FragCoord.xy / inputs.u_resolution;
-    outColor = vec4(st.x, 1 - st.y, 0.0, 1.0);
+    vec2 touch = vec2(inputs.u_pad_x, inputs.u_pad_y);
+    outColor = vec4(st.x, 1.0 - st.y, 0.0, 1.0);
 }
